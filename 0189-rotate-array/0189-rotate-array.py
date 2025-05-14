b64dbdb4e5c6cc8afdc03d806991609153bf6232
Aadithya2201 <145917810+Aadithya2201@users.noreply.github.com>
2025-05-14 22:54:02 +0530

    Time: 0 ms (100%), Space: 25.9 MB (9.76%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,8 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        d = deque(nums)
+        d.rotate(k)  
+        nums[:] = list(d) 
