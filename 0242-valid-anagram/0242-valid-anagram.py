daccd9afc7ae262df246c95e6e9b27352cc502e9
Aadithya2201 <145917810+Aadithya2201@users.noreply.github.com>
2025-06-29 22:16:22 +0530

    Time: 27 ms (25.53%), Space: 13.4 MB (41.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,3 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        s=sorted(s)
-        t=sorted(t)
-        return s==t
+class Solution(object):
+    def isAnagram(self, s, t):
+        return sorted(s)==sorted(t)
