ec87a6c739b4e7bcbdc4931be7ea314473f2fd72
Aadithya2201 <145917810+Aadithya2201@users.noreply.github.com>
2025-06-30 19:43:46 +0530

    Time: 2502 ms (57.36%), Space: 130.8 MB (35.38%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -6,7 +6,7 @@ class Solution(object):
         is_prime = [True] * n
         is_prime[0] = is_prime[1] = False
 
-        for i in range(2, int(n ** 0.5) + 1):
+        for i in range(2, int(math.sqrt(n)) + 1):
             if is_prime[i]:
                 for j in range(i * i, n, i):
                     is_prime[j] = False
