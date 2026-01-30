54fb4cdbca8021badd587c49f6f58ea5cb58a346
Aadithya.R <145917810+Aadithya2201@users.noreply.github.com>
2026-01-30 09:40:00 +0530

    Time: 0 ms (100%), Space: 42.5 MB (97.75%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,6 +1,22 @@
 class Solution {
     public String interpret(String command) {
-        StringBuilder s = new StringBuilder();
-        return command.replace("()","o").replace("(al)","al");
+        StringBuilder sb = new StringBuilder(command.length());
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'G') {
+                sb.append("G");
+            }
+            if (command.charAt(i) == '(') {
+                if (command.charAt(i+1) == ')') {
+                    sb.append("o");
+                    i++;
+                }
+                else {
+                    sb.append("al");
+                    i += 3;
+                }
+            }
+        }
+        
+        return sb.toString();
     }
 }
